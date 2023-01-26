f7dcad59c1e2eae2d0f8d939a32862a45d82d9d6
abmetheny <abmetheny@gmail.com>
2023-01-26 14:54:32 -0500

    Fixed adding/removing class relative to current time, added functionality to save textarea inputs to local storage

--- script.js
+++ script.js
@@ -1,68 +1,82 @@
-$(function () {
-  
-var currentDay = dayjs().format('dddd, MMMM D');
-$('#currentDay').text(currentDay);
+// $(function () {
+  var currentDay = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(currentDay);
 
-var time9 = dayjs().hour(9).format('ha');
-$('#time9').text(time9);
-var time10 = dayjs().hour(10).format('ha');
-$('#time10').text(time10);
-var time11 = dayjs().hour(11).format('ha');
-$('#time11').text(time11);
-var time12 = dayjs().hour(12).format('ha');
-$('#time12').text(time12);
-var time1 = dayjs().hour(13).format('ha');
-$('#time1').text(time1);
-var time2 = dayjs().hour(14).format('ha');
-$('#time2').text(time2);
-var time3 = dayjs().hour(15).format('ha');
-$('#time3').text(time3);
-var time4 = dayjs().hour(16).format('ha');
-$('#time4').text(time4);
-var time5 = dayjs().hour(17).format('ha');
-$('#time5').text(time5);
+  var time9 = dayjs().hour(9).format('ha');
+  $('#time9').text(time9);
+  var time10 = dayjs().hour(10).format('ha');
+  $('#time10').text(time10);
+  var time11 = dayjs().hour(11).format('ha');
+  $('#time11').text(time11);
+  var time12 = dayjs().hour(12).format('ha');
+  $('#time12').text(time12);
+  var time1 = dayjs().hour(13).format('ha');
+  $('#time1').text(time1);
+  var time2 = dayjs().hour(14).format('ha');
+  $('#time2').text(time2);
+  var time3 = dayjs().hour(15).format('ha');
+  $('#time3').text(time3);
+  var time4 = dayjs().hour(16).format('ha');
+  $('#time4').text(time4);
+  var time5 = dayjs().hour(17).format('ha');
+  $('#time5').text(time5);
 
-var timeArray = [time9, time10, time11, time12, time1, time2, time3, time4, time5]
-var currentHour = dayjs().format('H');
-var textClass = $('.text');
+  var timeArray = [time9, time10, time11, time12, time1, time2, time3, time4, time5]
+  var currentHour = dayjs().format('H');
+  var textClass = $('.text');
+  var buttonClass = $('.saveBtn');
 
-
-
-$(function () {
+  // $(function () {
   console.log(currentHour);
   console.log(timeArray);
+  console.log(textClass);
+  console.log(buttonClass);
 
   for (let i = 0; i < timeArray.length; i++) {
-    if (timeArray[i] == currentHour) {
+    buttonClass[i].addEventListener("click", function(){
+      console.log("clicked", (i+9));
+      // save textarea to local storage
+      var storedKey = (i+9);
+      console.log(storedKey);
+      var storedValue = textClass.val();
+      console.log(storedValue);
+      localStorage.setItem(storedKey, storedValue);
+      // populate textarea with local storage
+    })
+    if ((i+9) < currentHour) {
+      $(textClass[i]).addClass('past');
+      $(textClass[i]).removeClass('future');
+      $(textClass[i]).removeClass('present');
+      // console.log("past");
+    }
+
+    if ((i+9) > currentHour) {
+      $(textClass[i]).addClass('future');
+      $(textClass[i]).removeClass('past');
+      $(textClass[i]).removeClass('present');
+      // console.log("future")
+    }
+
+    if ((i+9) == currentHour) {
       $(textClass[i]).addClass('present');
       $(textClass[i]).removeClass('past');
       $(textClass[i]).removeClass('future');
+      // console.log("present");
     }
-    else {
-      if (timeArray[i] < currentHour) {
-        $(textClass[i]).addClass('past');
-        $(textClass[i]).removeClass('future');
-        $(textClass[i]).removeClass('present');
-      }
-      if (timeArray[i] > currentHour) {
-        $(textClass[i]).addClass('future');
-        $(textClass[i]).removeClass('past');
-        $(textClass[i]).removeClass('present');
-      }
-    }
+
   }
 
-})
+  // });
 
 
 
 
-});
+// });
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -81,4 +95,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
